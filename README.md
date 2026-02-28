ea8b609dbcae62a604192ff03b928e1d1d38f180
Aiden Taylor <ataylor3008@gmail.com>
2026-02-28 08:28:56 +0000

    Test

--- README.md
+++
@@ -1,2 +0,0 @@
-# Gateway2.0
-Second Version of The Gateway, build with Discord Integration from the ground up
--- terminals.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set "BASEDIR=%~dp0"
-
-wt -w 0 ^
-  new-tab --title "API"      -d "%BASEDIR%api"      powershell ^
-  ; new-tab --title "Frontend" -d "%BASEDIR%frontend" powershell ^
-  ; new-tab --title "Discord"  -d "%BASEDIR%discordbot"  powershell
